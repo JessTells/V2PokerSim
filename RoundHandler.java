8874b107362e8f0564267a21756bfeafec5e832f
JessTells <jernestot27@gmail.com>
2024-10-07 16:52:20 -0700

    added round handler idea

---
+++ RoundHandler.java
@@ -0,0 +1,16 @@
+public class RoundHandler {
+    /*
+        A simple round in Texas Holdem Poker:
+        Deal cards
+        p1 raises or checks or folds
+        p2 raises or checks or folds
+        ...
+        pn raises or checks or folds
+        
+        ^ This loops until all players have checked or all but 1 folded
+
+        If all but 1 player folds then that players gets the pot
+
+        If there are still players then the first card is revealed, process begins again
+     */
+}
